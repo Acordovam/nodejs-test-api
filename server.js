f162b147936db6c661289c2425d3a7d1946364f5
Alejandro Cordova <33092112+Acordovam@users.noreply.github.com>
2025-10-21 18:55:35 -0600

    Update response message for root route

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Desplegado en Render!');
+  res.send('¡Desplegado en Render y ahora estoy haciendo un cambio para probar los eventos Push en Render!');
 });
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor ESM corriendo en el puerto ${PORT}`);
